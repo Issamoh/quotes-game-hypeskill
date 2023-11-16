f931251783869d29d8b831c4e3b721d5299e9e0f
Issamoh <hm_ben_messaoud@esi.dz>
2023-11-16 23:11:53 +0100

    preparing a function responsible for making get requests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,18 +4,22 @@ import okhttp3.Response
 
 private val client =  OkHttpClient()
 
+fun getDataFrom(inputUrl: String) : String{
+    val request = Request.Builder()
+        .url(inputUrl)
+        .build()
 
+    client.newCall(request).execute().use { response ->
+        val jsonString = response.body!!.string()
+        return jsonString
+    }
+}
 
 
 fun main() {
     print("Enter quote ID: ")
     val quoteID = readLine()!!.toInt()
-    val request = Request.Builder()
-        .url("https://quotes-hyperskill.vercel.app/api/quotes/$quoteID")
-        .build()
-
-    client.newCall(request).execute().use { response ->
-        val jsonString = response.body!!.string()
-        println(jsonString)
-    }
+    val url = "https://quotes-hyperskill.vercel.app/api/quotes/$quoteID"
+    val response = getDataFrom(url)
+    print(response)
 }
